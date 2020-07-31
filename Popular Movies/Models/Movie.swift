7ab6534e1f8d23971de1e767a7f9eecd66471ace
Alvaro Gutierrez <gutierrezjacomealvaro@gmail.com>
2020-07-31 23:59:22 +0200

    Models updated

--- Popular Movies/Models/Movie.swift
+++ Popular Movies/Models/Movie.swift
@@ -10,22 +10,20 @@ import Foundation
 
 struct Movie {
     let adult: Bool
+    let backdropPath: String?
     let budget: Int
     let genres: [Genre]
-    let homepage: String
     let id: Int
-    let imdbID: String
     let originalLanguage: String
     let originalTitle: String
     let overview: String
     let popularity: Double
     let posterPath: String?
     let releaseDate: String
-    let revenue, runtime: Int
+    let revenue: Int
+    let runtime: Int
     let status: String
     let tagline: String
     let title: String
-    let video: Bool
-    let voteAverage: Double
     let voteCount: Int
 }
