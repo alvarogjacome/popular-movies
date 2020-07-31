55da3ddc22be96de3f398a43f5bb930eab44e301
Alvaro Gutierrez <alvaro.gutierrez@movetia.com>
2020-07-31 21:12:09 +0200

    Initial Commit

---
+++ Popular MoviesTests/Popular_MoviesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Popular_MoviesTests.swift
+//  Popular MoviesTests
+//
+//  Created by Álvaro Gutiérrez Jácome on 31/07/2020.
+//  Copyright © 2020 alvarogjacome. All rights reserved.
+//
+
+import XCTest
+@testable import Popular_Movies
+
+class Popular_MoviesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
